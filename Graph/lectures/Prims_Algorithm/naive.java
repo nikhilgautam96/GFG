b18ec229716d57d40cd7a3d70ce1e15dafb504a5
nikhil296 <nikhilgautam1729@gmail.com>
2022-11-12 17:48:49 +0530

    graph updated

--- Graph/lectures/Prims_Algorithm/naive.java
+++ Graph/lectures/Prims_Algorithm/naive.java
@@ -10,12 +10,14 @@ public class naive {
         minWeight[0] = 0; // starting vertex.
 
         for(int i = 0; i<V; i++) {
+            // Finding the minimum weight edge.
             int u = -1;
             for(int j = 0; j<V; j++) {
                 if(!mSet[j] && (u == -1 || minWeight[u] > minWeight[j])) {
                     u = j;
                 }
             }
+            // Updating the distance of all adjacents of 'U'.
             mSet[u] = true;
             res += minWeight[u];
             for(int j = 0; j<V; j++) {
