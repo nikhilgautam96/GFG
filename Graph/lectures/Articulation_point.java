8dd34424bc1c3c8149f099d386ae6c52a5da024c
nikhil296 <nikhilgautam1729@gmail.com>
2022-11-16 13:32:59 +0530

    graph updated

--- Graph/lectures/Articulation_point.java
+++ Graph/lectures/Articulation_point.java
@@ -1,5 +1,44 @@
+// Time : O(V + E), Space : O(V) - cuz we are doing simple DFS traversal.
 package Graph.lectures;
-
+import java.util.*;
 public class Articulation_point {
-    
+    void DFS(int cur, int parent, int timer, boolean visited[], int tin[], int low[], boolean isArticulation[], ArrayList<ArrayList<Integer>> adj) {
+        visited[cur] = true;
+        tin[cur] = low[cur] = timer++;
+        int individualChild = 0;
+        for(int it : adj.get(cur)) {
+            if(it == parent) continue;
+            if(!visited[it]) {
+                DFS(it, cur, timer, visited, tin, low, isArticulation, adj);
+                low[cur] = Math.min(low[cur], low[it]);
+                if(low[it] >= tin[cur] && parent != -1) {
+                    isArticulation[cur] = true;
+                }
+                individualChild++;
+            } else {
+                // Since this node(the neighbour node) is already visited, it can never be the Articulation point.
+                // bcz, we can reach this node even without traversing through the "cur" node.
+                low[cur] = Math.min(low[cur], tin[it]);
+            }
+        }
+        if(individualChild > 1 && parent == -1) {
+            // This means the starting node is also an articulation point bcz its children dont have a cross edge,
+            // or individual childs are not connected.
+            isArticulation[cur] = true;
+        }
+    }
+    void find_Articulation(ArrayList<ArrayList<Integer>> adj, int V) {
+        int tin[] = new int[V];
+        int low[] = new int[V];
+        boolean visited[] = new boolean[V];
+        // We use the below array to track articulation point bcz while doing DFS 1 node can become articulation point multiple times
+        // so instead of printing everytime we just mark it in an hash array.
+        boolean isArticulation[] = new boolean[V];
+        int timer = 0;
+        for(int i = 0; i<V; i++) {
+            if(!visited[i]) {
+                DFS(i, -1, timer, visited, tin, low, isArticulation, adj);
+            }
+        }
+    }
 }
--- Graph/lectures/Bridges.java
+++ Graph/lectures/Bridges.java
@@ -1,5 +1,34 @@
 package Graph.lectures;
 
+// Time : O(V + E), Space : O(V)
+import java.util.ArrayList;
 public class Bridges {
-    
+    void DFS(int cur, int parent, int timer, int tin[], int low[], boolean visited[], ArrayList<ArrayList<Integer>> adj) {
+        visited[cur] = true;
+        tin[cur] = low[cur] = timer++;
+
+        for(int it : adj.get(cur)) {
+            if(it == parent) continue;
+            if(!visited[it]) {
+                DFS(it, cur, parent, tin, low, visited, adj);
+                low[cur] = Math.min(low[cur], low[it]);
+                if(low[it] > tin[cur]) {
+                    System.out.println("Bridge between - "+cur+" : "+it);
+                }
+            }else {
+                low[cur] = Math.min(low[cur], tin[it]);
+            }
+        }
+    }
+    void find_bridge(ArrayList<ArrayList<Integer>> adj, int V) {
+        boolean visited[] = new boolean[V];
+        int tin[] = new int[V];
+        int low[] = new int[V];
+        int timer = 0;
+        for(int i = 0; i<V; i++) {
+            if(!visited[i]) {
+                DFS(i, -1, timer, tin, low, visited, adj);
+            }
+        }
+    }
 }
