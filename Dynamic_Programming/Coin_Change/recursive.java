f3ce3cdcbac9c59d9f6853254f7caed84c435362
nikhil296 <nikhilgautam1729@gmail.com>
2022-12-22 00:08:27 +0530

    Dp ++

---
+++ Dynamic_Programming/Coin_Change/recursive.java
@@ -0,0 +1,18 @@
+// Time : o(2 ^ n) - since we either select/ignore the coin.
+package Dynamic_Programming.Coin_Change;
+public class recursive {
+    int count_Ways(int coins[], int n, int sum) {
+        if(sum == 0) {
+            return 1; // we return '1' for one possible solution found.
+        }
+        if(n == 0 || sum < 0) {
+            return 0; // No solution possible.
+        }
+        // Else ->  { 1. we either select the coin - in this case number of coins still 
+        //                                          remains the same but the sum is 
+        //                                          reduced by the n'th coins value.
+        //            2. we don't select the coin - in this case we are left with 'n-1' coins
+        //                                          and the sum also remains the same. }
+        return count_Ways(coins, n, sum - coins[n-1]) + count_Ways(coins, n-1, sum);
+    }
+}
