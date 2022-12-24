cb4f4bfe2476132cf69db510f9ca03fe6da7c5f5
nikhil296 <nikhilgautam1729@gmail.com>
2022-12-24 16:16:51 +0530

    Dp ++

---
+++ Dynamic_Programming/Longest_Increasing_Subsequence/naive_using_dp.java
@@ -0,0 +1,20 @@
+// Time : Theta(n ^ 2), Space : Theta(n)
+package Dynamic_Programming.Longest_Increasing_Subsequence;
+public class naive_using_dp {
+    int LIS(int arr[], int n) {
+        int lis[] = new int[n];
+        for(int i = 0; i<n; i++) {
+            lis[i] =  1;
+            for(int j = 0; j<i; j++) {
+                if(arr[j] < arr[i]) {
+                    lis[i] = Math.max(lis[i], lis[j] + 1);
+                }
+            }
+        }
+        int res = lis[0];
+        for(int i = 1; i<n; i++) {
+            res = Math.max(res, lis[i]);
+        }
+        return res;
+    }
+}
