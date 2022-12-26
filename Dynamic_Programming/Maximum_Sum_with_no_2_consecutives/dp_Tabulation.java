e7674ff3b7203b70b37ffbe16f1c9e739b3697b9
nikhil296 <nikhilgautam1729@gmail.com>
2022-12-27 02:08:17 +0530

    Dp ++

---
+++ Dynamic_Programming/Maximum_Sum_with_no_2_consecutives/dp_Tabulation.java
@@ -0,0 +1,13 @@
+// Time : Theta(n), Space : Theta(n).
+package Dynamic_Programming.Maximum_Sum_with_no_2_consecutives;
+public class dp_Tabulation {
+    int maxSum(int arr[], int n) {
+        int dp[] = new int[n+1];
+        dp[0] = 0;
+        dp[1] = arr[0];
+        for(int i = 2; i<=n; i++) {
+            dp[i] = Math.max(arr[i-1] + dp[i-2], dp[i-1]);
+        }
+        return dp[n];
+    }
+}
