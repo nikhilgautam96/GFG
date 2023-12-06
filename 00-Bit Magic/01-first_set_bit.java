ef7d5a951ab7fbfa00e61b77f182cb47ba111eae
nikhilgautam96 <nikhilgautam1729@gmail.com>
2023-12-06 16:59:07 +0530

    Dsa completed

--- 00-Bit Magic/01-first_set_bit.java
+++ 00-Bit Magic/01-first_set_bit.java
@@ -1,5 +1,5 @@
 public class first_set_bit {
-    public static int getFirstSetBit(int n){            
+    public static int getFirstSetBit(int n) {            
         if(n == 0) return 0;
         int setBit = n - (n & (n-1)); // n ^ (n & (n-1))
         int pos = (int)(Math.log10(setBit)/Math.log10(2)); // log2(setBit)
